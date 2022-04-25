ae92620077518e5cfd6a1c8915e087ce2d9c9a1d
Camilo Chiang <48471579+Camilochiang@users.noreply.github.com>
2022-04-25 16:55:11 +0200

    Delete Software/ROS/build directory

--- Software/ROS/build/livox_ros_driver_external_IMU/livox_ros_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Agroscope/ASPEN/Software/ROS/src/livox_ros_driver_external_IMU/livox_ros_driver
-# Build directory: /home/ubuntu/Agroscope/ASPEN/Software/ROS/build/livox_ros_driver_external_IMU/livox_ros_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Software/ROS/build/realsense-ros-2.3.2/realsense2_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Agroscope/ASPEN/Software/ROS/src/realsense-ros-2.3.2/realsense2_camera
-# Build directory: /home/ubuntu/Agroscope/ASPEN/Software/ROS/build/realsense-ros-2.3.2/realsense2_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
